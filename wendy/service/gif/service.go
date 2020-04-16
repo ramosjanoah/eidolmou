246f283761fbe9122edf043836df6785e3fb8716
ramosbl <email@example.com>
2020-04-17 00:08:08 +0700

    Refactor more

--- wendy/service/gif/service.go
+++ wendy/service/gif/service.go
@@ -2,12 +2,13 @@ package gifservice
 
 import (
 	"github.com/ramosjanoah/eidolmou/wendy/repository"
+	"github.com/ramosjanoah/eidolmou/wendy/repository/actionbot"
 )
 
 // list all the repository needed for this service
-var ActionBotRepository repository.ActionBotRepository
+var ActionBot repository.ActionBotRepository
 
 // initialize the repository
 func init() {
-	ActionBotRepository := repository.GetActionBot()
+	ActionBot = actionbot.GetActionBot()
 }
