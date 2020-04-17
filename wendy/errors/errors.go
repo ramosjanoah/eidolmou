8c294cf222a23f92b34e7d33b8714e2f17f7a430
ramosbl <email@example.com>
2020-04-18 00:07:24 +0700

    update errors

--- wendy/errors/errors.go
+++ wendy/errors/errors.go
@@ -2,17 +2,26 @@ package errors
 
 import (
 	"fmt"
-	"github.com/bukalapak/apinizer/response"
 )
 
+type CustomError struct {
+	Message            string
+	SugarcoatedMessage string
+	HTTPCode           int
+}
+
+func (c *CustomError) Error() string {
+	return c.Message
+}
+
 func DontUnderstandError() error {
-	return response.CustomError{Message: "Sorry, I don't understand :(", HTTPCode: 403}
+	return &CustomError{Message: "Sorry, I don't understand :(", HTTPCode: 403}
 }
 
 func MissingParameterError(field string) error {
-	return response.CustomError{Message: fmt.Sprintf("Sorry, but you're missing this parameter: '%s'", field), HTTPCode: 400}
+	return &CustomError{Message: fmt.Sprintf("Sorry, but you're missing this parameter: '%s'", field), HTTPCode: 400}
 }
 
 func PayloadMissingError() error {
-	return response.CustomError{Message: "Sorry, but you sent the empty message", HTTPCode: 400}
+	return &CustomError{Message: "Sorry, but you sent the empty message", HTTPCode: 400}
 }
